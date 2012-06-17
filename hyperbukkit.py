2aff44b2af25a2d7bdba90609d3cf7b3151abcec
Niklas Korz <niklas@niklaskorz.de>
2012-06-17 12:04:06 +0200

    Added automatic creation of config directory and file if it doesn't exist

--- hyperbukkit.py
+++ hyperbukkit.py
@@ -1,8 +1,28 @@
 import org.bukkit as bukkit
 import org.yaml.snakeyaml as snakeyaml
+import os
+
+config_defaults = """address: 0.0.0.0
+port: 8080
+"""
 
 log = bukkit.Bukkit.getServer().getLogger()
+data_path = "plugins/" + info.getName()
+config_path = data_dir + "/config.yml"
 
 @hook.enable
 def onenable():
     log = pyplugin.getLogger()
+    yaml = snakeyaml.Yaml()
+
+    if !os.path.exists(data_path):
+        log.info("Creating data directory... ({})".format(data_path))
+        os.makedirs(data_path)
+    if !os.path.exists(config_path):
+        log.info("Creating config file... ({})".format(config_path))
+        with open(config_path, "w") as config_file:
+            config_file.write(config_defaults)
+
+    with open(config_path, "r") as config_file:
+        global config
+        config = yaml.load(config_file)
